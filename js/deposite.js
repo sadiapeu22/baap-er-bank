c1c77644c0bbb3763b0494f5d343f316797b5a06
sadiapeu22 <afripeu2000@gmail.com>
2025-01-27 13:18:35 +0600

    deposite and balance calculation

--- js/deposite.js
+++ js/deposite.js
@@ -4,12 +4,37 @@ document.getElementById('btn-deposite').addEventListener('click',function(){
     //console.log('clicked');
     //step 2 get the deposite amount from the deposite input field
     const depositeField = document.getElementById('user-deposite');
-    const depositeAmount = depositeField.value;
+    const newDepositeAmountString = depositeField.value;
+    const newDepositeAmount = parseFloat(newDepositeAmountString);
+    //console.log(typeof newDepositeAmount)
     //console.log(depositeAmount);
     //step 3 get the deposite current total amount
     //for non input(element other than input, textarea) use innerText to get the text
     const depositeTotalElement = document.getElementById('deposite-total');
-    const depositeTotal = depositeTotalElement.innerText;
-    depositeTotalElement.innerText = depositeAmount;
+    const previousDepositeTotalString = depositeTotalElement.innerText;
+    const previousDepositeTotal = parseFloat(previousDepositeTotalString);
+
+//step-4 add number to set the total deposite
+    const currentDepositeTotal = previousDepositeTotal + newDepositeAmount;
+
+    depositeTotalElement.innerText = currentDepositeTotal;
+    
+
+    //step-5 balance total amount
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    //step 6 calculate current total balance
+    const currentBalanceTotal = previousBalanceTotal + newDepositeAmount;
+    //set the balance total
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+
+
+
+
+     //step-7 clear the deposite field
+     depositeField.value = '';
 
 })
